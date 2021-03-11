877d01ee4637bf8cf585e17bab5ab6651a9f1857
Andrey Baglai <andreybaglai@gmail.com>
2021-03-11 09:46:42 +0200

    feat: add fullscreen functionality

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+const fullscreenBtn = document.querySelector('.fullscreen');
+
+fullscreenBtn.addEventListener('click', () => {
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen();
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    }
+  }
+});

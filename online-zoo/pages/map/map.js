f19413976b7deab32df835b9a1cb6f565e0e0c37
Andrey Baglai <andreybaglai@gmail.com>
2021-04-26 18:19:12 +0300

    fead: add slider on map page

--- online-zoo/pages/map/map.js
+++ online-zoo/pages/map/map.js
@@ -3,10 +3,17 @@ const changeThemeBtn = document.getElementById('changeTheme');
 const mobileIcon = document.getElementById('mobileIcon');
 const closeMobileIcon = document.getElementById('closeMobileNavIcon');
 let colorTheme = '';
-const map = document.getElementById('map')
-;
-const leftArrow = document.getElementById("leftArrow");
-const rightArrow = document.getElementById("rightArrow");
+const map = document.getElementById('map');
+const leftArrow = document.getElementById('leftArrow');
+const rightArrow = document.getElementById('rightArrow');
+
+const mapSliderItems = document.querySelectorAll('.item');
+const currentNumberElement = document.getElementById('map-slider-current-number');
+const range = document.getElementById('range');
+let currentIdx = 1;
+let animal = '';
+
+const watchOnlineBtn = document.getElementById('watchOnline');
 
 document.addEventListener('DOMContentLoaded', () => {
   colorTheme = getColorThemeFromLS();
@@ -57,7 +64,7 @@ changeThemeBtn.addEventListener('change', (e) => {
 function changeColorArrowsSlider() {
   const leftArrowPath = leftArrow.querySelector('path');
   const rightArrowPath = rightArrow.querySelector('path');
-  if (colorTheme === "dark") {
+  if (colorTheme === 'dark') {
     leftArrowPath.style.fill = '#ffffff';
     rightArrowPath.style.fill = '#ffffff';
   } else {
@@ -126,3 +133,141 @@ function getColorThemeFromLS() {
   return localStorage.getItem('theme');
 }
 /*Change theme color end*/
+
+/*Map markers start*/
+function removeActiveMarker() {
+  const markers = document.querySelectorAll('.map-marker');
+  markers.forEach((marker) => {
+    if (marker.classList.contains('active-marker')) {
+      marker.classList.remove('active-marker');
+    }
+  });
+}
+
+function setWatchOnlineBtnLink(animal) {
+  if (animal) {
+    watchOnlineBtn.href = `../zoos/${animal}/${animal}.html`;
+  } else {
+    watchOnlineBtn.href = `#`;
+  }
+}
+
+map.addEventListener('click', (e) => {
+  const target = e.target;
+
+  if (target.closest('.map-marker')) {
+    removeActiveMarker();
+    const marker = target.closest('.map-marker');
+    animal = marker.parentElement.title.toLowerCase();
+    currentIdx = +marker.parentElement.dataset.number;
+
+    removeActiveItem();
+    updateCurrentNumberEl();
+    setActiveItem();
+    setRangeValue();
+    setWatchOnlineBtnLink(animal);
+    marker.classList.add('active-marker');
+  }
+});
+/*Map markers end*/
+
+/*Map slider start*/
+function updateCurrentNumberEl() {
+  currentNumberElement.textContent = `0${currentIdx + 1}`;
+}
+
+function nextItem() {
+  currentIdx += 1;
+}
+
+function prevItem() {
+  currentIdx -= 1;
+}
+
+function removeActiveItem() {
+  mapSliderItems.forEach((item) => {
+    const img = item.querySelector('.active-li-wrapper img');
+    if (item.classList.contains('active-li-wrapper')) {
+      item.classList.remove('active-li-wrapper');
+      img.style.width = '108px';
+      img.style.height = '108px';
+    }
+  });
+}
+
+function setActiveItem() {
+  if (currentIdx < 0) {
+    currentIdx = mapSliderItems.length - 1;
+  }
+
+  if (currentIdx > mapSliderItems.length - 1) {
+    currentIdx = 0;
+  }
+
+  mapSliderItems.forEach((item, idx) => {
+    if (idx == currentIdx) {
+      const img = item.querySelector('img');
+      item.classList.add('active-li-wrapper');
+      img.style.width = '137px';
+      img.style.height = '137px';
+    }
+  });
+}
+
+function setRangeValue() {
+  range.value = currentIdx + 1;
+}
+
+function setAnimal() {
+  const name = document.querySelector('.active-li-wrapper').dataset.animal;
+  animal = name;
+}
+
+function setActiveMarker() {
+  const markers = document.querySelectorAll('.map-marker');
+  markers.forEach((marker) => {
+    if (marker.parentElement.title.toLocaleLowerCase() === animal) {
+      marker.classList.add('active-marker');
+    }
+  });
+}
+
+range.addEventListener('input', (e) => {
+  const val = e.target.value - 1;
+  currentIdx = val;
+  updateCurrentNumberEl();
+  removeActiveItem();
+  setActiveItem();
+
+  setAnimal();
+  removeActiveMarker();
+  setActiveMarker();
+  setWatchOnlineBtnLink(animal)
+});
+
+rightArrow.addEventListener('click', () => {
+  nextItem();
+  removeActiveItem();
+  setActiveItem();
+  updateCurrentNumberEl();
+  setRangeValue();
+
+  setAnimal();
+  removeActiveMarker();
+  setActiveMarker();
+  setWatchOnlineBtnLink(animal);
+});
+
+leftArrow.addEventListener('click', () => {
+  prevItem();
+  removeActiveItem();
+  setActiveItem();
+  updateCurrentNumberEl();
+  setRangeValue();
+
+  setAnimal();
+  removeActiveMarker();
+  setActiveMarker();
+  setWatchOnlineBtnLink(animal)
+});
+/*Map slider end*/
